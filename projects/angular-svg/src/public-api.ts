e92f52a16c5ed8abc6e3a2e316834eb36627dd7d
Aitor González Fernández <info@aitorgf.com>
2022-03-30 17:47:16 +0200

    :sparkles: Base lib updated to angular 9

--- projects/angular-svg/src/public-api.ts
+++ projects/angular-svg/src/public-api.ts
@@ -3,11 +3,19 @@
  */
 
 export * from './lib/types/index';
+export * from './lib/modules/svgcircle/svgcircle.component';
 export * from './lib/modules/svgcircle/svgcircle.module';
+export * from './lib/modules/svgellipse/svgellipse.component';
 export * from './lib/modules/svgellipse/svgellipse.module';
+export * from './lib/modules/svgline/svgline.component';
 export * from './lib/modules/svgline/svgline.module';
+export * from './lib/modules/svgpath/svgpath.component';
 export * from './lib/modules/svgpath/svgpath.module';
+export * from './lib/modules/svgpolygon/svgpolygon.component';
 export * from './lib/modules/svgpolygon/svgpolygon.module';
+export * from './lib/modules/svgpolyline/svgpolyline.component';
 export * from './lib/modules/svgpolyline/svgpolyline.module';
+export * from './lib/modules/svgrect/svgrect.component';
 export * from './lib/modules/svgrect/svgrect.module';
+export * from './lib/modules/svgtext/svgtext.component';
 export * from './lib/modules/svgtext/svgtext.module';
