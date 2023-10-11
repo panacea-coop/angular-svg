b2f573017639a583264bb0ae599c235c4b1b9373
Aitor González Fernández <info@aitorgf.com>
2023-10-11 20:48:28 +0200

    :sparkles: Add support for angular 14

--- projects/demo-page/src/app/app.module.ts
+++ projects/demo-page/src/app/app.module.ts
@@ -2,16 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {
-  SvgCircleModule,
-  SvgEllipseModule,
-  SvgLineModule,
-  SvgPathModule,
-  SvgPolygonModule,
-  SvgPolylineModule,
-  SvgRectModule,
-  SvgTextModule
-} from 'angular-svg';
+import {SvgCircleModule} from "../../../angular-svg/src/lib/modules/svgcircle/svgcircle.module";
+import {SvgEllipseModule} from "../../../angular-svg/src/lib/modules/svgellipse/svgellipse.module";
+import {SvgLineModule} from "../../../angular-svg/src/lib/modules/svgline/svgline.module";
+import {SvgPathModule} from "../../../angular-svg/src/lib/modules/svgpath/svgpath.module";
+import {SvgTextModule} from "../../../angular-svg/src/lib/modules/svgtext/svgtext.module";
+import {SvgRectModule} from "../../../angular-svg/src/lib/modules/svgrect/svgrect.module";
+import {SvgPolygonModule} from "../../../angular-svg/src/lib/modules/svgpolygon/svgpolygon.module";
+import {SvgPolylineModule} from "../../../angular-svg/src/lib/modules/svgpolyline/svgpolyline.module";
 
 @NgModule({
   declarations: [
